54fbf56d979989132fa78d169a82576a5b6b52dc
Alicia HAN <alicia.han007@gmail.com>
2020-06-03 21:13:21 +0200

    install vundle

--- modules/basic_settings/manifests/vim.pp
+++ modules/basic_settings/manifests/vim.pp
@@ -4,11 +4,26 @@ class basic_settings::vim
   $ultisnips_url = 'https://raw.githubusercontent.com/Aliciahan/Snippets/master/UltiSnips.tar.gz',
   $bundle_file_centos = 'https://github.com/Aliciahan/Snippets/blob/master/bundle-centos.tgz?raw=true',
   $vimrc_url = 'https://github.com/Aliciahan/Snippets/raw/master/.vimrc',
+  $vundle_git_url = 'https://github.com/VundleVim/Vundle.vim.git',
   
 ){
   case $facts['os']['name'] {
     'RedHat','CentOS': {
-      $common_vim_repo ='/usr/share/vim/vim80'
+
+      $common_vim_repo ='/usr/share/vim/vim80/plugin'
+
+      file {'prepare_common_vimrc_repo':
+        path   => $common_vim_repo,
+        ensure => directory,
+        mode   => '755',
+      }
+
+      file {'prepare_common_vim_repo':
+        path   => "${common_vim_repo}/UltiSnips",
+        ensure => directory,
+        mode   => '755',
+      }
+
     }
     /^(Debian|Ubuntu)$/: {
       $common_vim_repo ='/usr/share/vim/addons/plugin' 
@@ -58,11 +73,14 @@ class basic_settings::vim
     ensure => directory,
     mode   => '755',
   }
-  file {'prepare_common_vimrc_repo':
-    path   => "/etc/vim",
-    ensure => directory,
-    mode   => '755',
-  }
+
+
+  #file {'prepare_common_vimrc_repo':
+  #  path   => "/etc/vim",
+  #  ensure => directory,
+  #  mode   => '755',
+  #}
+
   archive { '/opt/UltiSnips.tgz': 
     ensure            => present,
     extract           => true,
@@ -88,6 +106,13 @@ class basic_settings::vim
     source    => $vimrc_url,
     provider  => 'wget',
     require   =>  File['prepare_common_vimrc_repo'],
+  }
+
+  exec {'manuall-install-vundle':
+    cwd     => ${common_vim_repo},
+    command => "git clone ${vundle_git_url} ${common_vim_repo}/bundle/Vundle.vim && touch /etc/vundle_installed",
+    path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+    onlyif => "test ! -e /etc/vundle_installed",
   }
 
   file {'colorls setting up':
