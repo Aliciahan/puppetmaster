082c13e91bf7f7d67eea2279e1a62e5fe0dcf30e
aliciahan <alicia.han07@gmail.com>
2020-06-05 07:55:05 +0200

    update

--- modules/basic_settings/manifests/init.pp
+++ modules/basic_settings/manifests/init.pp
@@ -2,8 +2,6 @@ class basic_settings
 (
 ){
 
-  include basic_settings::vim
-
   file {'/etc/profile.d/alicia_profile.sh': 
     content => alternative_template("basic_settings", "alicia_profile.sh.erb"),
     mode    => '0755',
